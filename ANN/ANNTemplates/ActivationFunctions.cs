9dd911307a3de1d92425a2e36e9f345b46939360
Nathan Chinn <nchinn@hotmail.com>
2017-11-02 18:54:14 -0600

    Added Tanh and Softmax activation Functions Added Tanh gradient function

--- ANN/ANNTemplates/ActivationFunctions.cs
+++ ANN/ANNTemplates/ActivationFunctions.cs
@@ -8,5 +8,42 @@ namespace ANN.ANNTemplates
 {
     class ActivationFunctions
     {
+        public static double Tanh(double input)
+        {
+            if (input < -20.0) return -1.0F; // approximation is correct to 30 decimals
+            else if (input > 20.0) return 1.0F;
+            else return (float)Math.Tanh(input);
+        }
+
+        public static double SoftMax(double input, double[] other)
+        {
+            double retval = 0;
+            double denominator = 0;
+            object lockobject = new object();
+            Parallel.ForEach(
+                other,
+                () => 0.0d,
+                (n, loopState, partialResult) =>
+                {
+                    return Math.Exp(n) + partialResult;
+                },
+                (localPartialSum) =>
+                {
+                    lock (lockobject)
+                    {
+                        denominator += localPartialSum;
+                    }
+                });
+
+            try
+            {
+                retval = Math.Exp(input) / denominator;
+            }
+            catch
+            {
+                retval = -999;
+            }
+            return retval;
+        }
     }
 }
--- ANN/ANNTemplates/GradientFunctions.cs
+++ ANN/ANNTemplates/GradientFunctions.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ANN.ANNTemplates
 {
-    class GradientFunctions
+    public static class GradientFunctions
     {
+        public static double Tanh(double input)
+        {
+            if ((input < -15.0) || (input > 15.0)) return 0.0F; // approximation is correct to 30 decimals
+            else if (input == 0) return 1.0F;
+            else return (Math.Tanh(input) * (1 - Math.Tanh(input)));
+        }
     }
 }
